901c0607c3de912a04c802385f2c290aa2e0da79
mohammadnawaz-590028338 <MOHAMMAD.28338@stu.upes.ac.in>
2025-12-02 01:56:38 +0530

    Create Set9Q3.c

---
+++ Set9Q3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    char line[200];
+
+    fp = fopen("myfile.txt", "r");
+
+    if (fp == NULL) {
+        printf("File not found!");
+        return 1;
+    }
+
+    while (fgets(line, sizeof(line), fp)) {
+        printf("%s", line);
+    }
+
+    fclose(fp);
+    return 0;
+}
